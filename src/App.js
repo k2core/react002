5446531ca3b5c2275af289a202edd5e14a17b630
k2core <7179062@gmail.com>
2023-12-22 14:15:01 +0900

    완성
    Changes to be committed:
    modified:   src/App.js
    new file:   src/components/Clicker.js
    new file:   src/components/Header.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Clicker from "./components/Clicker";
 
 function App() {
-  return <div className="App">App.js</div>;
+  const [title] = useState("Clicker");
+  return (
+    <div className="App">
+      <Header title={title} />
+      <Clicker />
+    </div>
+  );
 }
 
 export default App;
